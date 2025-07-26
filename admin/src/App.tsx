8d3e635e8146c3d490cae68a1df887858ac98ce6
k.anisimov <k.anisimov@epicentrk.ua>
2025-07-26 20:30:36 +0300

    FC

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,3 +1,3 @@
 import { HydraAdmin } from "@api-platform/admin";
 
-export const App = () =>  <HydraAdmin entrypoint="http://16.171.161.215/api" />;
+export const App = () =>  <HydraAdmin entrypoint="https://16.171.161.215/api" />;
